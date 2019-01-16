746845a3a18f99cab0cc9e10115a02844e26a503
HannahKang <hannahkang109@gmail.com>
2019-01-16 11:45:01 +0900

    1일차: 주석추가

--- src/main/java/kr/green/spring/HomeController.java
+++ src/main/java/kr/green/spring/HomeController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
@@ -34,6 +33,11 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		
+		/*컨트롤러에서 jsp로 데이터를 전달하기 위해서
+		 * 1. 매개변수에 Model 객체를 추가 : Model model
+		 * 2. model.addAttribute()를 통해 데이터 전달
+		 * 	  model.addAttribute("jsp에서 받는 이름", 전달할 데이터);*/
+		
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
@@ -41,11 +45,11 @@ public class HomeController {
 		
 		@RequestMapping(value = "/", method = RequestMethod.POST)
 		public String homePost(String name) {
+			/* jsp에서 넘겨준 데이터를 받기 위해 태그의 속성 name과 일치하는 이름을 매개변수의 이름으로 설정*/
 			System.out.println("jsp에서 넘어온 값 :" + name);
 			
 			return "redirect:/";
 		
-
 	}
 
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -8,9 +8,12 @@
 <h1>
 	Hello world!  
 </h1>
-
+<%-- 컨트롤러에서 전달한 데이터를 받기 위해 ${컨트롤러에서 전달한이름}을 사용한다. --%>
 <P>  The time on the server is ${serverTime}. </P>
-<form method="post">
+<%-- jsp에서 컨트롤러로 데이터를 전달하려면 form태그를 이용 --%>
+<%--<form method="post" action="/spring/">이렇게 이름을 넣어줄 수 있지만 이름 바뀔때마다 바꿔줘야 하므로 하기와 같이함--%>
+<form method="post" action="<%=request.getContextPath() %>/">
+	<%-- 전달하려는 데이터가 있는 태그의 속성 name을 입력 --%>
 	<input type="text" name="name">
 	<button>제출</button>
 </form>	
