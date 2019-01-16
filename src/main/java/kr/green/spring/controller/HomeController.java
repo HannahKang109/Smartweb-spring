0b695fbca947e9a48ffefeace3e4df732b50373d
HannahKang <hannahkang109@gmail.com>
2019-01-16 16:36:29 +0900

    1일차: DB연동 및 회원가입 예제구현

--- src/main/java/kr/green/spring/controller/HomeController.java
+++ src/main/java/kr/green/spring/controller/HomeController.java
@@ -1,4 +1,4 @@
-package kr.green.spring;
+package kr.green.spring.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -6,10 +6,14 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.green.spring.service.AccountService;
+import kr.green.spring.vo.AccountVo;
 
 /**
  * Handles requests for the application home page.
@@ -19,6 +23,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@Autowired
+	private AccountService accountService;
+	
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -39,6 +46,7 @@ public class HomeController {
 		 * 	  model.addAttribute("jsp에서 받는 이름", 전달할 데이터);*/
 		
 		model.addAttribute("serverTime", formattedDate );
+		System.out.println(accountService.getAccount("hannahkang109"));
 		
 		return "home";
 	}
@@ -47,9 +55,26 @@ public class HomeController {
 		public String homePost(String name) {
 			/* jsp에서 넘겨준 데이터를 받기 위해 태그의 속성 name과 일치하는 이름을 매개변수의 이름으로 설정*/
 			System.out.println("jsp에서 넘어온 값 :" + name);
-			
 			return "redirect:/";
 		
 	}
-
+		@RequestMapping(value = "/signup", method = RequestMethod.GET)
+		public String signupGet(Boolean fail, Model model) {
+			if(fail == null) {
+				fail = false;
+			}
+			model.addAttribute("fail", fail);
+			return "account/signup";
+		}
+		//int -> Integer, double -> Double, boolean -> Boolean(null값 허용) Wrapper 클래스
+		@RequestMapping(value = "/signup", method = RequestMethod.POST)
+		public String signupPost(AccountVo accountVo, Model model) {
+			if(accountService.signup(accountVo))
+				return "redirect:/";
+			else {
+				model.addAttribute("fail",true);
+				return "redirect:/signup";
+			}
+			
+		}
 }
